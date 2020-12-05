d696e71fb8bbd68e1ff8e1b010907c039d722679
romakv4 <rkalmykov@hwdtech.com>
2020-12-06 01:54:56 +0600

    config app name like as gh repo name

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/recipes-frontend'),
+      dir: require('path').join(__dirname, './coverage/recipes-frontend-public'),
       subdir: '.',
       reporters: [
         { type: 'html' },
