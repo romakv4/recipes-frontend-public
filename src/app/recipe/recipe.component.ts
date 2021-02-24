d40e8d8834d2307ca9b34e9a0805dcb5b5361161
romakv4 <romakv4@gmail.com>
2021-02-24 21:49:20 +0600

    add share button to recipe

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../types/Recipe';
 
 @Component({
   selector: 'app-recipe',
@@ -9,11 +10,32 @@ export class RecipeComponent {
 
   @Input() recipe;
 
+  isShareSupported: boolean = false;
   fullRecipeVisibility = false;
 
-  constructor() { }
+  constructor() {
+    this.isShareSupported = navigator.share != undefined;
+  }
 
   toggleFDVisibility() {
     this.fullRecipeVisibility = !this.fullRecipeVisibility;
+  }
+
+  shareRecipe() {
+    navigator.share({
+      text: this.getRecipeTextPresentationForSharing()
+    });
+  }
+
+  getRecipeTextPresentationForSharing() {
+    let recipeFullDescriptionRepresentation = "";
+    this.recipe.fullDescription.forEach((step, index) => {
+      if (index !== this.recipe.fullDescription.length - 1) {
+        recipeFullDescriptionRepresentation += `${index + 1}) ${step}\n`;
+      } else {
+        recipeFullDescriptionRepresentation += `${index + 1}) ${step}`;
+      }
+    });
+    return `${this.recipe.name}\n${this.recipe.category}\n${this.recipe.cookingTime}\n${recipeFullDescriptionRepresentation}`;
   }
 }
